b0b08b8341675d69774e7a71911390f01dc7731e
minmin8-lee <59409959+minmin8-lee@users.noreply.github.com>
2020-01-11 17:34:40 +0900

    Update Container.py

--- Stack/Container.py
+++ Stack/Container.py
@@ -1,33 +1,36 @@
 class Container:
-	def __init__(self.):
-		self._data = None
-		self._size = None
-		self._count = None
-		self._cursor = None
-	
-	def peek(self):
-		return _cursor
-	
-	def add_count(self):
-		self._count += 1
-	
-	def sub_count(self):
-		self._count -= 1
-	
-	def push(self):
-		pass
-	
-	def pop(self):
-		pass
-		
-	def is_full(self):
-		if _count == _size:
-			return True
-		else:
-			return False
-	
-	def is_empty(self):
-		if _count == 0:
-			return True
-		else:
-			return False
+    def __init__(self):
+        self.__data__ = []
+        self._size = None
+        self._count = 0
+        self._index = self._count - 1
+        self._cursor = None
+
+    def _reset_index(self):
+        self._index = self._count - 1
+
+    def _add_count(self):
+        self._count += 1
+        self._reset_index()
+
+    def _sub_count(self):
+        self._count -= 1
+        self._reset_index()
+
+    def peek(self):
+        return self._cursor
+
+    def count(self):
+        return self._count
+
+    def is_full(self):
+        if self._count == self._size:
+            return True
+        else:
+            return False
+
+    def is_empty(self):
+        if self._count == 0:
+            return True
+        else:
+            return False
