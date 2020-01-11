293cad8c8594967df6cacc4d8f497f2b7d8d11e7
minmin8-lee <59409959+minmin8-lee@users.noreply.github.com>
2020-01-11 15:44:57 +0900

    Create Container.py
    Container for stack and queue

---
+++ Container.py
@@ -0,0 +1,33 @@
+class Container:
+	def __init__(self.):
+		self._data = None
+		self._size = None
+		self._count = None
+		self._cursor = None
+	
+	def peek(self):
+		return _cursor
+	
+	def add_count(self):
+		self._count += 1
+	
+	def sub_count(self):
+		self._count -= 1
+	
+	def push(self):
+		pass
+	
+	def pop(self):
+		pass
+		
+	def is_full(self):
+		if _count == _size:
+			return True
+		else:
+			return False
+	
+	def is_empty(self):
+		if _count == 0:
+			return True
+		else:
+			return False
