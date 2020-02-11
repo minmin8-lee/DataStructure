46d904acac65dba2977069dc02e42bd88c5f34b7
minmin8-lee <59409959+minmin8-lee@users.noreply.github.com>
2020-02-11 23:59:19 +0900

    Create Quick.py
    퀵정렬

---
+++ Sort/Quick.py
@@ -0,0 +1,49 @@
+
+
+class Quick:
+    """
+    Quick Sort
+        ~ for python3 list ~
+        tutorialspoint.com/data_structures_algorithms/quick_sort_algorithm.htm
+    """
+    def __init__(self, data=None):
+        self._data = data
+
+    def mount(self, data):
+        self._data = data
+
+    def _swap(self, pos1, pos2):
+        temp = self._data[pos1]
+        self._data[pos1] = self._data[pos2]
+        self._data[pos2] = temp
+
+    def _pivot(self, pivot, left, right):
+        while True:
+            while self._data[left] < self._data[pivot]:
+                left += 1
+                continue
+            while self._data[right] > self._data[pivot] and right > 0:
+                right -= 1
+                continue
+
+            if left >= right:
+                break
+            else:
+                self._swap(left, right)
+
+        self._swap(left, pivot)
+        return left
+
+    def _quick_sort(self, left, right):
+
+        if right <= left:
+            return
+        else:
+            pivot = right
+            boundary = self._pivot(pivot, left, right-1)
+            self._quick_sort(left, boundary-1)
+            self._quick_sort(boundary+1, right)
+
+    def sort(self):
+        self._quick_sort(0, len(self._data)-1)
+        return self._data
