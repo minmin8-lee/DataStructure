f1f750dd1d7cbe9d71d3c8291694d112af8a2d87
minmin8-lee <59409959+minmin8-lee@users.noreply.github.com>
2020-02-29 21:15:46 +0900

    Update TreeTestDrive.py
    AVLTree 업데이트

--- TreeTestDrive.py
+++ TreeTestDrive.py
@@ -12,3 +12,15 @@ bst.search(11)
 bst.in_order_traversal()  # BST에서 중위순회시 오름차순이 됨
 bst.pre_order_traversal()
 bst.post_order_traversal()
+
+
+from Tree.AVLTree import AVLT
+
+avl = AVLT()
+lst = random_list_generator(0, 10, 30)
+for n in lst:
+    avl.insert(n)
+
+avl.in_order_traversal()  # BST에서 중위순회시 오름차순이 됨
+avl.pre_order_traversal()
+avl.post_order_traversal()
