e7d77627e3f3bc4eeb729a2b0cef4f309c63ebf1
minmin8-lee <59409959+minmin8-lee@users.noreply.github.com>
2020-02-29 21:15:03 +0900

    Update Node.py
    균형트리에서 사용가능한 Node 업데이트

--- Tree/Node.py
+++ Tree/Node.py
@@ -4,7 +4,9 @@ class Node:
         self.__data__ = data
         self.__left__ = None
         self.__right__ = None
+        self.__parent__ = None
         self.__level__ = None
+        self.__bf__ = 0
 
     def _set_data_(self, data):
         self.__data__ = data
@@ -16,6 +18,42 @@ class Node:
     def set_right(self, data, cnt):
         self.__right__ = Node(data)
         self.__right__.set_level(cnt)
+
+    def change_left(self, node):
+        if isinstance(node, Node):
+            self.__left__ = node
+        elif node is None:
+            self.__left__ = None
+        else:
+            raise TypeError
+
+    def change_right(self, node):
+        if isinstance(node, Node):
+            self.__right__ = node
+        elif node is None:
+            self.__right__ = None
+        else:
+            raise TypeError
+
+    def set_parent(self, node):
+        # AVL Tree의 경우 부모노드의 정보를 갖고있어야 BF 계산시 time complexity 감소 가능
+        # 데이터 1개분의 공간복잡도 상승은 덤
+        if isinstance(node, Node):
+            self.__parent__ = node
+        elif node is None:
+            self.__parent__ = None
+        else:
+            raise TypeError
+
+    def set_bf(self, val):
+        # dangerous ! be careful !!
+        self.__bf__ = val
+
+    def add_bf(self):
+        self.__bf__ += 1
+
+    def sub_bf(self):
+        self.__bf__ -= 1
 
     def set_level(self, cnt):
         self.__level__ = cnt
@@ -31,3 +69,16 @@ class Node:
 
     def get_level(self):
         return self.__level__
+
+    def get_parent(self):
+        return self.__parent__
+
+    def get_bf(self):
+        return self.__bf__
+
+    def is_empty(self):
+        if self.get_left() is None and self.get_right() is None:
+            return True
+
+        else:
+            return False
