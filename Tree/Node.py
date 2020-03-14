da376b1eeba4f3550529cc4e5a03fed09ebf8423
minmin8-lee <59409959+minmin8-lee@users.noreply.github.com>
2020-03-14 21:30:43 +0900

    Update Node.py
    modified for RBT

--- Tree/Node.py
+++ Tree/Node.py
@@ -6,7 +6,12 @@ class Node:
         self.__right__ = None
         self.__parent__ = None
         self.__level__ = None
+
+        # for AVLT
         self.__bf__ = 0
+
+        # for RBT
+        self.__color__ = None
 
     def _set_data_(self, data):
         self.__data__ = data
@@ -79,6 +84,51 @@ class Node:
     def is_empty(self):
         if self.get_left() is None and self.get_right() is None:
             return True
-
         else:
             return False
+
+    def is_left_child(self):
+        if self.get_parent() is None:
+            raise KeyError("엄마없음")
+        if self.get_parent().get_left() == self:
+            return True
+        elif self.get_parent().get_right() == self:
+            return False
+        else:
+            raise ValueError("왼쪽자식도 오른쪽자식도 아니면 누구자식? 메모리오류")
+
+    """
+    Belows required for RBT Implementation
+    """
+
+    def get_grand_parent(self):
+        if self.get_parent() is None:
+            return None
+        else:
+            # parent 없는 경우가 처리되었으므로, parent의 parent가 없는 경우는 알아서 return None. 호출문재X
+            return self.get_parent().get_parent()
+
+    def get_uncle(self):
+        if self.get_parent() is None:
+            return None
+        elif self.get_grand_parent() is None:
+            return None
+        else:
+            # 내 부모가 왼쪽 자식이면 할배의 오른쪽이 삼촌
+            if self.get_parent().is_left_child():
+                return self.get_grand_parent().get_right()
+            # 내 부모가 오른쪽 자식이면 할배의 왼쪽이 삼촌
+            else:
+                return self.get_grand_parent().get_left()
+
+    def get_color(self):
+        if self.__color__ is None:
+            return False
+        else:
+            return self.__color__
+
+    def set_color(self, color):
+        if isinstance(color, bool):
+            self.__color__ = color
+        else:
+            raise TypeError("got wrong color. check color type. True: Red, False: Black")
